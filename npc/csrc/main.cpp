86a851fef29d95d1e673971eff48f24689c026e9
tracer-ysyx <tracer@ysyx.org>
2024-08-14 14:02:08 +0800

    > sim RTL ysyx_22040000 朱宏明 Linux aqua 5.15.0-116-generic #126-Ubuntu SMP Mon Jul 1 10:14:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:02:08 up 27 days, 21:06,  1 user,  load average: 1.88, 1.82, 1.81

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,35 @@
-#include <stdio.h>
+#include <Vtop.h>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+#include <verilated.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
+int main(int argc, char *argv[]) {
+  const std::unique_ptr<VerilatedContext> ctxp = std::make_unique<VerilatedContext>();
+  ctxp->commandArgs(argc, argv);
+  const std::unique_ptr<Vtop> top = std::make_unique<Vtop>(ctxp.get(), "TOP");
+
+  int count = 10;
+  while (count--) {
+    ctxp->timeInc(1);
+
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
+  }
+
+  // Final model cleanup
+  top->final();
+
+  // Final simulation summary
+  ctxp->statsPrintSummary();
+
+  // Return good completion status
+  // Don't use exit() or destructor's may hang
   return 0;
 }
