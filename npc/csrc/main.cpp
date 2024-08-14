2acb9e6035df66c5ee9d9d69672a836388ff268c
tracer-ysyx <tracer@ysyx.org>
2024-08-14 16:07:03 +0800

    > sim RTL ysyx_22040000 朱宏明 Linux aqua 5.15.0-116-generic #126-Ubuntu SMP Mon Jul 1 10:14:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:07:03 up 27 days, 23:11,  1 user,  load average: 1.72, 2.57, 2.71

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,43 +1,28 @@
 #include <Vtop.h>
-#include <cassert>
-#include <cstdio>
-#include <cstdlib>
-#include <memory>
-#include <verilated.h>
-#include <verilated_vcd_c.h>
+#include <nvboard.h>
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME *top);
+
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
+
+static void reset(int n) {
+  dut.rst = 1;
+  while (n-- > 0) single_cycle();
+  dut.rst = 0;
+}
 
 int main(int argc, char *argv[]) {
-  const std::unique_ptr<VerilatedContext> contextp = std::make_unique<VerilatedContext>();
-  contextp->commandArgs(argc, argv);
-  const std::unique_ptr<Vtop> top = std::make_unique<Vtop>(contextp.get(), "TOP");
-  Verilated::traceEverOn(true);
-  VerilatedVcdC* tfp = new VerilatedVcdC;
-  top->trace(tfp, 99); // Trace 99 levels of hierarchy
-  tfp->open("build/dump.vcd");
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+  reset(10);
 
-  const int sim_time = 10;
-  while (contextp->time() < sim_time && !contextp->gotFinish()) {
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    assert(top->f == (a ^ b));
-    tfp->dump(contextp->time());
-    contextp->timeInc(1);
-
+  while (1) {
+    nvboard_update();
+    single_cycle();
   }
-  tfp->close();
-  delete tfp;
-
-  // Final model cleanup
-  top->final();
-
-  // Final simulation summary
-  contextp->statsPrintSummary();
-
-  // Return good completion status
-  // Don't use exit() or destructor's may hang
-  return 0;
 }
