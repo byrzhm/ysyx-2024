13f0545fcfde0e54f074a5c29e2987c5c22096fb
tracer-ysyx <tracer@ysyx.org>
2024-08-16 15:06:48 +0800

    > compile NEMU ysyx_22040000 朱宏明 Linux aqua 5.15.0-116-generic #126-Ubuntu SMP Mon Jul 1 10:14:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:06:48 up 29 days, 22:11,  1 user,  load average: 0.13, 0.19, 0.18

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -35,12 +35,21 @@
 #define PMEM64 1
 #endif
 
-typedef MUXDEF(CONFIG_ISA64, uint64_t, uint32_t) word_t;
-typedef MUXDEF(CONFIG_ISA64, int64_t, int32_t)  sword_t;
+#ifdef CONFIG_ISA64
+typedef uint64_t word_t;
+typedef int64_t sword_t;
+#else
+typedef uint32_t word_t;
+typedef int32_t sword_t;
+#endif
 #define FMT_WORD MUXDEF(CONFIG_ISA64, "0x%016" PRIx64, "0x%08" PRIx32)
 
 typedef word_t vaddr_t;
-typedef MUXDEF(PMEM64, uint64_t, uint32_t) paddr_t;
+#ifdef PMEM64
+typedef uint64_t paddr_t;
+#else
+typedef uint32_t paddr_t;
+#endif
 #define FMT_PADDR MUXDEF(PMEM64, "0x%016" PRIx64, "0x%08" PRIx32)
 typedef uint16_t ioaddr_t;
 
